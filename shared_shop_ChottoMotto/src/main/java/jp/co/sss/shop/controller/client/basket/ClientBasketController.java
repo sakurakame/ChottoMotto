a5cf6e3ea1d513817e5755cc4cd5b4b5119e94fa
Kozyrev Roman <kozyrev.roman@pkutech.co.jp>
2023-05-22 17:03:20 +0900

    ClientBasketController <- メソッド追加

--- shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -1,5 +1,61 @@
 package jp.co.sss.shop.controller.client.basket;
 
-public class ClientBasketController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.repository.ItemRepository;
+
+@Controller
+public class ClientBasketController {
+	@Autowired
+	private ItemRepository repository;
+
+	private List<Item> basketItems;
+
+	@RequestMapping(path = "/client/basket/list")
+	public String showItemList(Model model) {
+		List<Item> items = repository.findAll();
+		model.addAttribute("items", items);
+		model.addAttribute("basketItems", basketItems);
+		return "client/basket/list";
+	}
+
+	@RequestMapping(path = "/client/basket/add")
+	public String addItemToBasket(@RequestParam("itemId") Integer itemId) {
+		Item item = repository.findById(itemId).orElse(null);
+		if (item != null) {
+			basketItems.add(item);
+		}
+
+		return "redirect:/client/basket/list";
+	}
+
+	@RequestMapping(path = "/client/basket/delete")
+	public String deleteItemFromBasket(@RequestParam("itemId") Integer itemId) {
+		Item itemToRemove = null;
+		for (Item item : basketItems) {
+			if (item.getId().equals(itemId)) {
+				itemToRemove = item;
+				break;
+			}
+		}
+
+		if (itemToRemove != null) {
+			basketItems.remove(itemToRemove);
+		}
+
+		return "redirect:/client/basket/list";
+	}
+
+	@RequestMapping(path = "/client/basket/allDelete")
+	public String deleteAllItemsFromBasket() {
+		basketItems.clear();
+		return "redirect:/client/basket/list";
+	}
 }
