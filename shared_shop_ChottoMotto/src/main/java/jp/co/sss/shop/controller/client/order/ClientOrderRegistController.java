2d5e8056ab2af4b82cb624f63fd990ad3d56ade4
Minegami Seiji <minegami.seiji@pkutech.co.jp>
2023-05-24 14:51:45 +0900

    峯上　決済処理　更新　5/24/14:51

--- shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.bean.UserBean;
 import jp.co.sss.shop.form.OrderForm;
 import jp.co.sss.shop.repository.UserDao;
 
@@ -20,23 +20,24 @@ public class ClientOrderRegistController {
 	@Autowired
 	UserDao userDao;
 
-	@RequestMapping(path = "/client/order/address/input", method = RequestMethod.POST)
-	public String handleOrderAddressInput(Model model, HttpSession session) {
-		// 注文入力フォーム情報の初期化
+	@Autowired
+	HttpSession session;
+
+	@RequestMapping(path = "/client/order/address/inpt", method = RequestMethod.GET)
+	public String handleOrderAddressInput(Model model) {
+		
+		
+		System.out.println("こんにちわ");
+		
+		// orderFormオブジェクトを作成
 		OrderForm orderForm = new OrderForm();
 
 		// セッションスコープからログイン会員情報を取得
-		User user = (User) session.getAttribute("user");
+		orderForm.setId(((UserBean) session.getAttribute("user")).getId());
 
-		// ユーザIDを条件にDBからユーザ情報を取得
-		User userInfo = userDao.findByIdAndDeleteFlag(user.getId(), 0);
-
-		// 取得したユーザ情報を注文入力フォーム情報に設定
-		orderForm.setId(userInfo.getId());
-
-		// 注文入力フォーム情報の支払方法に初期値としてクレジットカードを設定
+		// 注文入力フォーム情報の支払方法に初期値とsしてクレジットカードを設定
 		orderForm.setPayMethod(1);
-
+		
 		// 注文入力フォーム情報をセッションスコープに保存
 		session.setAttribute("orderForm", orderForm);
 
@@ -45,18 +46,21 @@ public class ClientOrderRegistController {
 	}
 
 	@RequestMapping(value = "/client/order/address/input", method = RequestMethod.GET)
-	public String showAddressInputPage(Model model, HttpSession session) {
+	public String showAddressInputPage(Model model) {
 		// セッションスコープから注文入力フォーム情報を取得
 		OrderForm orderForm = (OrderForm) session.getAttribute("orderForm");
+		
+		System.err.println("こんばんは");
+		BindingResult result = (BindingResult) session.getAttribute("result");
+		if (result != null) {
+			//セッションにエラー情報がある場合、エラー情報をスコープに設定
+			model.addAttribute("org.springframework.validation.BindingResult.orderForm", result);
+			// セッションのエラー情報は削除
+			session.removeAttribute("result");
+		}
 
 		// 注文入力フォーム情報をリクエストスコープに設定
 		model.addAttribute("orderForm", orderForm);
-
-		// セッションスコープに入力エラー情報がある場合、エラー情報を取得しリクエストスコープに設定し、セッションスコープから削除
-		if (session.getAttribute("errorInfo") != null) {
-			model.addAttribute("errorInfo", session.getAttribute("errorInfo"));
-			session.removeAttribute("errorInfo");
-		}
 
 		// 登録画面表示処理にフォワード
 		return "client/order/address_input";
@@ -107,52 +111,52 @@ public class ClientOrderRegistController {
 		// 注文確認画面表示処理にリダイレクト
 		return "redirect:/client/order/check";
 	}
-//
-//	@RequestMapping(value = "/client/order/check", method = RequestMethod.GET)
-//	public String showOrderCheckPage(Model model, HttpSession session) {
-//		// セッションスコープから注文情報を取得
-//		Order order = (Order) session.getAttribute("order");
-//
-//		// セッションスコープから買い物かご情報を取得
-//		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
-//
-//		// 注文商品の最新情報をDBから取得し、在庫チェックをする
-//		List<OrderItem> orderItems = order.getOrderItems();
-//		for (OrderItem item : orderItems) {
-//			// 最新の在庫情報をDBから取得
-//			Product product = productService.getProductById(item.getProductId());
-//
-//			// 在庫チェックと注文数の調整
-//			if (item.getQuantity() > product.getStock()) {
-//				// 在庫不足の場合は警告メッセージを設定し、注文数を在庫数に調整
-//				model.addAttribute("warningMessage", "在庫が不足しています");
-//				item.setQuantity(product.getStock());
-//			}
-//		}
-//
-//		// 在庫切れ商品の削除
-//		orderItems.removeIf(item -> item.getQuantity() <= 0);
-//
-//		// 在庫状況を反映した買い物かご情報をセッションに保存
-//		session.setAttribute("cart", cart);
-//
-//		// 買い物かご情報から商品ごとの金額小計と合計金額を算出し、注文入力フォーム情報に設定
-//		BigDecimal subtotal = BigDecimal.ZERO;
-//		for (CartItem cartItem : cartItems) {
-//			BigDecimal itemPrice = cartItem.getProduct().getPrice()
-//					.multiply(BigDecimal.valueOf(cartItem.getQuantity()));
-//			subtotal = subtotal.add(itemPrice);
-//		}
-//		BigDecimal total = subtotal.add(shippingFee); // 送料を合計金額に加算
-//		orderForm.setSubtotal(subtotal);
-//		orderForm.setTotal(total);
-//
-//		// 注文入力フォーム情報をリクエストスコープに設定
-//		model.addAttribute("orderForm", orderForm);
-//
-//		// 注文確認画面を表示する
-//		return "client/order/check";
-//	}
+	//
+	//	@RequestMapping(value = "/client/order/check", method = RequestMethod.GET)
+	//	public String showOrderCheckPage(Model model, HttpSession session) {
+	//		// セッションスコープから注文情報を取得
+	//		Order order = (Order) session.getAttribute("order");
+	//
+	//		// セッションスコープから買い物かご情報を取得
+	//		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+	//
+	//		// 注文商品の最新情報をDBから取得し、在庫チェックをする
+	//		List<OrderItem> orderItems = order.getOrderItems();
+	//		for (OrderItem item : orderItems) {
+	//			// 最新の在庫情報をDBから取得
+	//			Product product = productService.getProductById(item.getProductId());
+	//
+	//			// 在庫チェックと注文数の調整
+	//			if (item.getQuantity() > product.getStock()) {
+	//				// 在庫不足の場合は警告メッセージを設定し、注文数を在庫数に調整
+	//				model.addAttribute("warningMessage", "在庫が不足しています");
+	//				item.setQuantity(product.getStock());
+	//			}
+	//		}
+	//
+	//		// 在庫切れ商品の削除
+	//		orderItems.removeIf(item -> item.getQuantity() <= 0);
+	//
+	//		// 在庫状況を反映した買い物かご情報をセッションに保存
+	//		session.setAttribute("cart", cart);
+	//
+	//		// 買い物かご情報から商品ごとの金額小計と合計金額を算出し、注文入力フォーム情報に設定
+	//		BigDecimal subtotal = BigDecimal.ZERO;
+	//		for (CartItem cartItem : cartItems) {
+	//			BigDecimal itemPrice = cartItem.getProduct().getPrice()
+	//					.multiply(BigDecimal.valueOf(cartItem.getQuantity()));
+	//			subtotal = subtotal.add(itemPrice);
+	//		}
+	//		BigDecimal total = subtotal.add(shippingFee); // 送料を合計金額に加算
+	//		orderForm.setSubtotal(subtotal);
+	//		orderForm.setTotal(total);
+	//
+	//		// 注文入力フォーム情報をリクエストスコープに設定
+	//		model.addAttribute("orderForm", orderForm);
+	//
+	//		// 注文確認画面を表示する
+	//		return "client/order/check";
+	//	}
 
 	@RequestMapping(value = "/client/order/payment/back", method = RequestMethod.POST)
 	public String redirectToPaymentInput() {
@@ -160,34 +164,34 @@ public class ClientOrderRegistController {
 		return "redirect:/client/order/payment/input";
 	}
 
-//	@RequestMapping(value = "/client/order/complete", method = RequestMethod.POST)
-//	public String completeOrder(HttpSession session) {
-//		// セッションスコープから注文情報を取得
-//		Order order = (Order) session.getAttribute("order");
-//
-//		// セッションスコープから買い物かご情報を取得
-//		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
-//
-//		// 注文商品の在庫チェックをする
-//		List<OrderItem> orderItems = order.getOrderItems();
-//		for (OrderItem item : orderItems) {
-//			// 在庫切れ商品がある場合は、注文確認画面表示処理にリダイレクト
-//			Product product = productService.getProductById(item.getProductId());
-//			if (item.getQuantity() > product.getStock()) {
-//				return "redirect:/client/order/check";
-//			}
-//		}
-//
-//		// 注文情報を元にDB登録用エンティティオブジェクトを生成し、注文テーブルおよび注文商品テーブルに登録を実施
-//		orderService.placeOrder(order);
-//
-//		// セッションスコープの注文入力フォーム情報と買い物かご情報を削除
-//		session.removeAttribute("order");
-//		session.removeAttribute("cart");
-//
-//		// 注文完了画面表示処理にリダイレクト
-//		return "redirect:/client/order/complete";
-//	}
+	//	@RequestMapping(value = "/client/order/complete", method = RequestMethod.POST)
+	//	public String completeOrder(HttpSession session) {
+	//		// セッションスコープから注文情報を取得
+	//		Order order = (Order) session.getAttribute("order");
+	//
+	//		// セッションスコープから買い物かご情報を取得
+	//		ShoppingCart cart = (ShoppingCart) session.getAttribute("cart");
+	//
+	//		// 注文商品の在庫チェックをする
+	//		List<OrderItem> orderItems = order.getOrderItems();
+	//		for (OrderItem item : orderItems) {
+	//			// 在庫切れ商品がある場合は、注文確認画面表示処理にリダイレクト
+	//			Product product = productService.getProductById(item.getProductId());
+	//			if (item.getQuantity() > product.getStock()) {
+	//				return "redirect:/client/order/check";
+	//			}
+	//		}
+	//
+	//		// 注文情報を元にDB登録用エンティティオブジェクトを生成し、注文テーブルおよび注文商品テーブルに登録を実施
+	//		orderService.placeOrder(order);
+	//
+	//		// セッションスコープの注文入力フォーム情報と買い物かご情報を削除
+	//		session.removeAttribute("order");
+	//		session.removeAttribute("cart");
+	//
+	//		// 注文完了画面表示処理にリダイレクト
+	//		return "redirect:/client/order/complete";
+	//	}
 
 	@RequestMapping(value = "/client/order/complete", method = RequestMethod.GET)
 	public String showOrderCompletePage() {
@@ -196,5 +200,3 @@ public class ClientOrderRegistController {
 	}
 
 }
-
-
