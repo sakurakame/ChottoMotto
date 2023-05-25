c3c3dec5c01ab9f24253c3c01a8a5f02d921eafd
Kozyrev Roman <kozyrev.roman@pkutech.co.jp>
2023-05-25 16:21:48 +0900

    ClientBasketController -> 90% basket/list.html -> 90% ItemShowController -> 70%

--- shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop_ChottoMotto/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -30,7 +30,6 @@ public class ClientBasketController {
 	
 	@RequestMapping(path = "/client/basket/list")
 	public String showItemList(HttpSession session, Model model) {
-		
 		@SuppressWarnings({ "unchecked"})
 		List<BasketBean> myBaskets = (List<BasketBean>)session.getAttribute("myBaskets");
 		session.setAttribute("myBaskets", myBaskets);
@@ -50,41 +49,63 @@ public class ClientBasketController {
 				myBaskets = new ArrayList<BasketBean>();
 			}
 			
+			boolean itemExists = false;
+		    for (BasketBean basketItem : myBaskets) {
+		        if (basketItem.getId().equals(item.getId())) {
+		            basketItem.setOrderNum(basketItem.getOrderNum() + 1);
+		            itemExists = true;
+		            break;
+		        }
+		    }
+		    
+		    if (!itemExists) {
 			BasketBean basketBean = new BasketBean();
 			basketBean.setId(item.getId());
 			basketBean.setName(item.getName());
 			basketBean.setStock(item.getStock());
+			basketBean.setOrderNum(1);
 			myBaskets.add(basketBean);
 			
-			session.setAttribute("myBaskets", myBaskets);
-			model.addAttribute("myBaskets", myBaskets);
+		}
+		    
+		session.setAttribute("myBaskets", myBaskets);
+		model.addAttribute("myBaskets", myBaskets);
+			
 			
 		return "client/basket/list";
 	}
 	
 	@RequestMapping(path = "/client/basket/delete")
-	public String deleteItemFromBasket(@RequestParam("itemId") Integer itemId, Model model) {
-		List<Item> items = repository.findAll();
-		Item itemToRemove = null;
-		for (Item item : items) {
-			if (item.getId().equals(itemId)) {
-				itemToRemove = item;
-				break;
-			}
-		}
+	public String deleteItemFromBasket(@RequestParam("id") Integer id, HttpSession session, Model model) {
+		@SuppressWarnings("unchecked")
+		List<BasketBean> myBaskets = (List<BasketBean>)session.getAttribute("myBaskets");
+		
+		if (myBaskets != null) {
+	        BasketBean itemToRemove = null;
+	        for (BasketBean basket : myBaskets) {
+	            if (basket.getId().equals(id)) {
+	                if (basket.getOrderNum() > 1) {
+	                    basket.setOrderNum(basket.getOrderNum() - 1);
+	                } else {
+	                    itemToRemove = basket;
+	                }
+	                break;
+	            }
+	        }
+	        if (itemToRemove != null) {
+	            myBaskets.remove(itemToRemove);
+	        }
 
-		if (itemToRemove != null) {
-			items.remove(itemToRemove);
-		}
-		model.addAttribute("items",items);
+	        session.setAttribute("myBaskets", myBaskets);
+	    }
 		return "redirect:/client/basket/list";
 	}
 
 	@RequestMapping(path = "/client/basket/allDelete")
-	public String deleteAllItemsFromBasket(Model model) {
-		List<Item> items = repository.findAll();
-		items.clear();
-		model.addAttribute("items",items);
+	public String deleteAllItemsFromBasket(HttpSession session, Model model) {
+		@SuppressWarnings("unchecked")
+		List<BasketBean> myBaskets = (List<BasketBean>)session.getAttribute("myBaskets");
+		myBaskets.removeAll(myBaskets);
 		return "redirect:/client/basket/list";
 	}
 }
